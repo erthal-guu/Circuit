5fcf57f01e516d05a455d48a5a6ff1473cd42ac7
Erthal-guu <vplgugs@gmail.com>
2025-12-23 16:11:35 -0300

    Commit inicial com a estrutura do Maven

---
+++ Circuit/Circuit/src/test/java/Circuit/Circuit/CircuitApplicationTests.java
@@ -0,0 +1,13 @@
+package Circuit.Circuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
