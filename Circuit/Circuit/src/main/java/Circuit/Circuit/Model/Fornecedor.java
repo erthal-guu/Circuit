7e214f899496ab629ebd0fcb7f9b52b8f1453973
Erthal-guu <vplgugs@gmail.com>
2026-01-08 16:25:32 -0300

    Criação dos arquivos para o crud de fornecedores

---
+++ Circuit/Circuit/src/main/java/Circuit/Circuit/Model/Fornecedor.java
@@ -0,0 +1,43 @@
+package Circuit.Circuit.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "fornecedores")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fornecedor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_fantasia", nullable = false, length = 150)
+    private String nomeFantasia;
+
+    @Column(name = "razao_social", nullable = false, length = 150)
+    private String razaoSocial;
+
+    @Column(nullable = false, unique = true, length = 18)
+    private String cnpj;
+
+    @Column(length = 15)
+    private String telefone;
+
+    @Column(nullable = false)
+    private String email;
+
+
+    @Column(length = 9)
+    private String cep;
+
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+}
---
+++ Circuit/Circuit/src/main/java/Circuit/Circuit/Repository/FornecedorRepository.java
@@ -0,0 +1,8 @@
+package Circuit.Circuit.Repository;
+
+import Circuit.Circuit.Model.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+
+}
