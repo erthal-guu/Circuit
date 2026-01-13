d61d93d52068348f5e4cc76e4da759654bbb4f0b
Erthal-guu <vplgugs@gmail.com>
2026-01-13 17:22:44 -0300

    Mudança para thymeLeaf

--- Circuit/Circuit/src/main/resources/static/js/estoque.js
+++ Circuit/Circuit/src/main/resources/static/js/estoque.js
@@ -1,8 +1,31 @@
+
 const modal = document.getElementById('estoqueModal');
 const formEstoque = document.getElementById('estoqueForm');
 
-function switchTab(tabName){
-document.querySelectorAll('.tab-content').forEach(c => c.classlist.remove('active'));
-document.querySelectorAll('.tab-btn').forEach(b => b.classlist.remove('active'));
+function switchTab(tabName) {
+    document.querySelectorAll('.tab-content').forEach(c => c.classList.remove('active'));
+    document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
 
+    if (tabName === 'ativos') {
+        document.getElementById('tabAtivos').classList.add('active');
+    } else {
+        document.getElementById('tabInativos').classList.add('active');
+    }
+    if (event) {
+        event.currentTarget.classList.add('active');
+    }
+}
+function openModal() {
+    if (modal) {
+        modal.classList.add('active');
+    }
+}
+function closeModal() {
+    if (modal) {
+        modal.classList.remove('active');
+    }
+    if (formEstoque) formEstoque.reset();
+    const preview = document.querySelector('.image-upload-label img');
+    if (preview) preview.remove();
+    document.getElementById('fileNameDisplay').innerText = "Nenhum arquivo selecionado";
 }
